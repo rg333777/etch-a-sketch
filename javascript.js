e2900e0df970d9f720e1a47ba55903462a2dd45c
rg333777 <143126002+rg333777@users.noreply.github.com>
2024-10-16 15:07:16 +0100

    added grid generation function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
 const container = document.querySelector('.container')
+let grid_size = 16
+
+function generateGrid(grid_size) {
+    for (let i = 0; i < grid_size -1; i++) {
+        const column = document.createElement('div')
+        for (let i = 0; i < grid_size -1; i++) {
+            const row = document.createElement('div')
+            row.style.height = `${600/grid_size}px`
+            row.style.width = `${600/grid_size}px`
+            row.style.border = 'thin, solid, black'
+            row.classList.add('square')
+            column.appendChild(row)
+        }
+    container.appendChild(column)
+    }
+}
+
+generateGrid(grid_size)
+
