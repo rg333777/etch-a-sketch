1a3c2296df82a4fea89c6712675f8dcef680b18b
rg333777 <143126002+rg333777@users.noreply.github.com>
2024-10-16 17:16:02 +0100

    added the new grid feature

--- javascript.js
+++ javascript.js
@@ -1,30 +1,33 @@
-const container = document.querySelector('.container')
-let grid_size = 16
+
+const grid_size = 16;
 
 function generateGrid(grid_size) {
-    for (let i = 0; i < grid_size -1; i++) {
-        const column = document.createElement('div')
-        for (let i = 0; i < grid_size -1; i++) {
-            const row = document.createElement('div')
-            row.style.height = `${600/grid_size}px`
-            row.style.width = `${600/grid_size}px`
-            row.style.border = 'thin, solid, black'
-            row.classList.add('square')
-            column.appendChild(row)
+    const container = document.querySelector('.container');
+    for (let i = 1; i <= grid_size; i++) {
+        const column = document.createElement('div');
+        for (let i = 1; i <= grid_size; i++) {
+            const row = document.createElement('div');
+            row.style.height = `${600/grid_size}px`;
+            row.style.width = `${600/grid_size}px`;
+            row.style.border = 'thin, solid, black';
+            row.classList.add('square');
+            row.addEventListener('mouseover', () => {
+                row.style.backgroundColor = 'red'
+            })
+            column.appendChild(row);
         }
-    container.appendChild(column)
+    container.appendChild(column);
     }
 }
 
 generateGrid(grid_size)
 
-function changeColor() {
-    let square = document.querySelectorAll('.square')
-    square.forEach((square) => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'red'
-        }) 
-    })
-}
-
-changeColor()
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    const container = document.querySelector('.container');
+    const new_grid_size = prompt('what length would you like the grid to be? (100 is the max)');
+    container.innerHTML = '';
+    generateGrid(new_grid_size)
+    }
+    
+) 
